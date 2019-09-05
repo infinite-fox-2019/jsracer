67639bbc6681a893d4a3d3663602d5cb72fb2b61
devitasari <devitas700@gmail.com>
2019-09-05 11:32:14 +0700

    add new file

--- js-racer.js
+++ js-racer.js
@@ -1,40 +1,78 @@
-"use strict"
-
-function diceRoll () {
-
-}
-
-function sleep (milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
+function generateBoard(pemain, lintasan) {
+  let board = []
+  for (let i=0; i<pemain; i++) {
+    board.push([])
+    for (let j=0; j<lintasan; j++) {
+      board[i].push(' ')
     }
   }
+  return board
 }
 
-function printBoard () {
 
+function start(board, jumlahPemain, calonPemain) {
+  for (let j=0; j<jumlahPemain; j++) {
+    board[j][0] = calonPemain[j]
+  }
+  return board
+}
+  
+function main(board, calonPemain, jumlahPemain) {
+  let posisiAwal
+  let posisiAkhir
+
+  for (let i=0; i<jumlahPemain; i++) {
+    posisiAwal = board[i].indexOf(calonPemain[i])
+    board[i][posisiAwal] = ' '
+    let hasilDadu = dadu()
+    posisiAkhir = posisiAwal + hasilDadu
+    if (posisiAkhir > board[i].length-1) posisiAkhir = board[i].length-1
+    board[i][posisiAkhir] = calonPemain[i]
+    sleep(1000)
+    for (j=0; j<jumlahPemain; j++) {
+      console.log(board[j].join('|'))
+    }
+    //cek apakah sudah ada pemenang
+    if (board[i][board[i].length-1] == calonPemain[i]) {
+      return `Pemenangnya adalah ${calonPemain[i]}`
+    } 
+    
+  }
+  
+  return main(board, calonPemain, jumlahPemain)
+}
+  
+function dadu() {
+  return Math.ceil(Math.random()*3)
 }
 
-function printLine (player, pos) {
-
-}
-
-function advance (player) {
-
-}
-
-function finished () {
-
-}
-
-function winner () {
-
-}
-
-function clearScreen () {
+function clearScreen() {
   // Un-comment this line if you have trouble with console.clear();
-  // return process.stdout.write('\033c');
+  return process.stdout.write('\033c');
   console.clear();
 }
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+          clearScreen();
+          break;
+        }
+    }
+}
+  
+function utama() {
+  let pemain = process.argv[2]
+  let lintasan = process.argv[3]
+  if (pemain < 2 || pemain > 26 || lintasan < 15) return 'Invalid input'
+  let board = generateBoard(pemain, lintasan)
+  let calonPemain = 'abcdefghijklmnopqrstuvwxyz'
+  let jumlahPemain = board.length
+  board = start(board, jumlahPemain, calonPemain)
+  for (let i=0; i<jumlahPemain; i++) {
+    console.log(board[i].join('|'))
+  }
+  return main(board, calonPemain,jumlahPemain )
+}
+
+console.log(utama())
