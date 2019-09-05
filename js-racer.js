8a07e1094215da6fb2d827baca64a4e072f7dd7b
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-05 17:59:12 +0700

    :anchor:

--- js-racer.js
+++ js-racer.js
@@ -1,39 +1,98 @@
 "use strict"
 
-function diceRoll () {
+// const playersArg = Number(process.argv[2])
+const rute = Number(process.argv[3])
+const players = [
+  {
+    name: 'a',
+    pos: 0
+  },
+  {
+    name: 'b',
+    pos: 0
+  },
+  // {
+  //   name: 'c',
+  //   pos: 0
+  // },
+]
 
+function diceRoll() {
+  return Math.ceil(Math.random() * 3)
 }
 
-function sleep (milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
+function sleep(milliseconds) {
+  let start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
     if ((new Date().getTime() - start) > milliseconds) {
       break;
     }
   }
 }
 
-function printBoard () {
+function printBoard() {
+  const board = []
+
+  for (let i = 0; i < players.length; i++) {
+    let distance = []
+    for (let j = 0; j < rute; j++) {
+      // if (j === 0) distance.push(`${players[i].name}`)
+      distance.push(' ')
+    }
+    board.push(distance)
+  }
+
+  return board
+}
+
+function printLine(player, pos) {
+  let isFinish = false
+  let win = ''
+  const board = printBoard()
+
+  while (!isFinish) {
+    for (let i = 0; i < players.length; i++) {
+      const dice = diceRoll()
+      board[i][players[i].pos] = players[i].name
+
+      if ((players[i].pos + dice) > board[i].length - 1) {
+        players[i].pos = rute - 1
+      } else {
+        players[i].pos += dice
+      }
+
+      board[i][players[i].pos] = players[i].name
+      board[i][players[i].pos - dice] = ' '
+      
+      if (winner(board[i], players[i].name) || finished(board[i], players[i].name)) {
+        win = `${players[i].name} win`
+        isFinish = true
+      }
+      clearScreen()
+      console.log(board, players)
+      sleep(500)
+    }
+  }
+  console.log(win)
+}
+console.log(printLine())
+
+
+function advance(player) {
 
 }
 
-function printLine (player, pos) {
-
+function finished(board, player) {
+  if (board[board.length - 1] == player) return true
+  return false
 }
 
-function advance (player) {
-
+function winner(board, player) {
+  if (board[board.length - 1] == player) return true
+  return false
 }
 
-function finished () {
-
-}
-
-function winner () {
-
-}
-
-function clearScreen () {
+function clearScreen() {
   // Un-comment this line if you have trouble with console.clear();
   // return process.stdout.write('\033c');
   console.clear();
