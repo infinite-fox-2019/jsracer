f9aab735d44180d0841b430180598d79efd2a519
ItsJoel88 <imanuelnjodi@gmail.com>
2019-09-05 19:45:22 +0700

    mengumpulkan js-racer

--- js-racer.js
+++ js-racer.js
@@ -1,7 +1,18 @@
 "use strict"
+let pemain = process.argv[2]
+let board = process.argv[3]
+let kamus = 'abcdefghijklmnopqrstuvwxyz'
+let pemainArr = []
+for(let i=0; i<pemain; i++){
+  let obj = {}
+  obj.name = kamus[i]
+  obj.position = 1
+  pemainArr.push(obj)
+}
 
 function diceRoll () {
-
+  let dice = Math.floor(Math.random()*6)+1
+  return dice
 }
 
 function sleep (milliseconds) {
@@ -13,24 +24,62 @@ function sleep (milliseconds) {
   }
 }
 
-function printBoard () {
-
+function printBoard (arrObj) {
+  let l = 0
+  let tampung = []
+  let result = []
+  if(board < 15){
+    return 'Minimal lintasan adalah 15'
+  }
+  while(l < pemain){
+    for(let i=0; i<board; i++){
+      tampung.push('|')
+      if(arrObj[l].position === i){
+        tampung.push(arrObj[l].name) 
+      }else{
+        tampung.push(' ')
+      }
+    }
+    result.push(tampung.join(''))
+    tampung = []
+    l+=1
+  }
+  return result.join('\n')
 }
 
 function printLine (player, pos) {
-
 }
 
-function advance (player) {
-
+function advance (players) {
+  if(players.length<2){
+    return 'jumlah minimal adalah 2'
+  }
+  let isFinished = false
+  while(!isFinished){
+    for(let i=0; i<players.length; i++){
+      players[i].position += diceRoll()
+      isFinished = finished(players[i])
+      clearScreen()
+      console.log(printBoard(players))
+      sleep(1000)
+      if(isFinished===true){
+        winner(players[i].name)
+        break
+      }
+    }
+  }
 }
 
-function finished () {
-
+function finished (player) {
+  if(player.position >= board-1){
+    player.position = board-1
+    return true
+  }
+  return false
 }
 
-function winner () {
-
+function winner (win) {
+  console.log(`And The Winner is....${win}`)
 }
 
 function clearScreen () {
@@ -38,3 +87,5 @@ function clearScreen () {
   // return process.stdout.write('\033c');
   console.clear();
 }
+
+console.log(advance(pemainArr))
