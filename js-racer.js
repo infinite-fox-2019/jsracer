56da5847bbbe39487270fdb6f7a59ae3b5d9e7b6
Ricky <audrickong.ao@gmail.com>
2019-09-05 18:20:36 +0700

    Upload

--- js-racer.js
+++ js-racer.js
@@ -1,7 +1,12 @@
 "use strict"
+const listOfPlayers = 'abcdefghijklmnopqrstuvwxyz'
+const numOfPlayers = Number(process.argv[2])
+const roadLength = Number(process.argv[3])
+const players = []
 
 function diceRoll () {
-
+  let randomDicer = Math.floor((Math.random() * 6) + 1)
+  return randomDicer
 }
 
 function sleep (milliseconds) {
@@ -13,24 +18,47 @@ function sleep (milliseconds) {
   }
 }
 
-function printBoard () {
-
+function printLine (player, position) {
+  const lane = []
+  for(let i = 0; i < roadLength; i++){
+    lane.push(' ')
+  }
+  lane[position] = player
+  console.log(lane.join('|'))
 }
 
-function printLine (player, pos) {
-
+function printBoard () {
+  for(let i = 0; i < players.length; i++) {
+    printLine(players[i].name, players[i].position)
+  }
 }
 
 function advance (player) {
-
+  let advancement = diceRoll()
+  if(player.position + advancement >= roadLength){
+    player.position = roadLength - 1
+  }
+  else{
+    player.position += advancement
+  }
 }
 
 function finished () {
-
+  for(let i = 0; i < players.length; i++){
+    if(players[i].position === (roadLength - 1)){
+      return true
+    }
+  }
+  return false
 }
 
 function winner () {
-
+  for(let i = 0; i < players.length; i++){
+    if(players[i].position === (roadLength - 1)){
+      return players[i].name
+    }
+  }
+  return false
 }
 
 function clearScreen () {
@@ -38,3 +66,36 @@ function clearScreen () {
   // return process.stdout.write('\033c');
   console.clear();
 }
+
+function startRacer(){
+  if(numOfPlayers < 2) {
+    return "Why so lonely, get some more players!"
+  }
+  if(roadLength < 15) {
+    return "Are you sure you wanna race this quick?!"
+  }
+    for(let i = 0; i < numOfPlayers; i++){
+      players.push({
+        name: listOfPlayers[i],
+        position: 0
+      })
+    }
+    let isFinished = false
+    clearScreen()
+    while(!isFinished){
+      for(let i = 0; i < players.length; i++){
+        advance(players[i])
+        printBoard()
+        sleep(500)
+        clearScreen()
+        isFinished = finished()
+        if(isFinished){
+          break
+        }
+      }
+    }
+    printBoard()
+    return `Congratulations! ${winner()} won the game!!`
+}
+
+console.log(startRacer())
